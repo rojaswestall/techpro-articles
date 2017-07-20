0e017371e6fb9802fdef7c434161153b366e44a2
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-07-19 23:51:26 -0500

    Articles from Northwestern Insight have been compiled. Still need to remove all the ones that are over 2 weeks old

---
+++ nuarticles.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+
+####
+#### Northwestern Insight KELLOGG School of Managment Website ####
+####
+
+reqStrategy = requests.get('https://insight.kellogg.northwestern.edu/browse/topic/strategy')
+soupStrategy = BeautifulSoup(reqStrategy.text, 'html.parser')
+
+reqCareers = requests.get('https://insight.kellogg.northwestern.edu/browse/topic/careers')
+soupCareers = BeautifulSoup(reqCareers.text, 'html.parser')
+
+reqCulture = requests.get('https://insight.kellogg.northwestern.edu/browse/topic/culture')
+soupCulture = BeautifulSoup(reqCulture.text, 'html.parser')
+
+reqLeadership = requests.get('https://insight.kellogg.northwestern.edu/browse/topic/leadership')
+soupLeadership = BeautifulSoup(reqLeadership.text, 'html.parser')
+
+reqTrust = requests.get('https://insight.kellogg.northwestern.edu/browse/topic/trust')
+soupTrust = BeautifulSoup(reqTrust.text, 'html.parser')
+#Only pulling articles from the strategy, careers, culture, leadership, and trust categories
+
+nulinks = []
+
+for li in soupStrategy.find_all('li', class_ = 'with-image'):
+	atag = li.find_next('a')
+	link = atag.get('href')
+	nulinks.append(link)
+for li in soupCareers.find_all('li', class_ = 'with-image'):
+	atag = li.find_next('a')
+	link = atag.get('href')
+	nulinks.append(link)
+for li in soupCulture.find_all('li', class_ = 'with-image'):
+	atag = li.find_next('a')
+	link = atag.get('href')
+	nulinks.append(link)
+for li in soupLeadership.find_all('li', class_ = 'with-image'):
+	atag = li.find_next('a')
+	link = atag.get('href')
+	nulinks.append(link)
+for li in soupTrust.find_all('li', class_ = 'with-image'):
+	atag = li.find_next('a')
+	link = atag.get('href')
+	nulinks.append(link)
+#Adds the second half of the url for an article to nulinks
+
+nulinks = list(set(nulinks))
+nulinks = ['https://insight.kellogg.northwestern.edu' + link for link in nulinks]
+#nulinks is now a list of urls for all useful articles on the site without any repeats
+
+#Want to remove all the article that are over two weeks old
+
+print(nulinks)
